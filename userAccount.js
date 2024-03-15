ae6b445f85c7a8eab467c2598c4a31811778cf14
Anna Shysh <95748448+dirrgin@users.noreply.github.com>
2024-03-15 11:16:23 +0100

    Update userAccount.js with mongoose validators

--- userAccount.js
+++ userAccount.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');//import the mongoose library to interact w
 const Transaction = require('./Transaction.js');
 const Group = require('./Group.js');
 
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
 const userAccountsSchema = new mongoose.Schema({
   _id: {
     type: objectId,
@@ -10,17 +15,31 @@ const userAccountsSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    required: true,
+    required: 'Email address is required',
+    trim: true,
     unique: true,
+     validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
   },
   nickname: {
     type: String,
     required: true,
     unique: true,
+    validate: {
+      validator: function(value) {
+        return value.length >= 1 && value.length <= 30;
+      }, message: 'Nickname must be between 1 and 30 characters long'
+    } 
   },
   password: {
     type: String,
+    trim: true,// Trims leading and trailing whitespace
     required: true,
+    validate: {
+      validator: function(value) {
+        return value.length >= 8 && value.length <= 30;
+      }, message: 'Password must be between 8 and 30 characters long'
+    } 
   },
   profilePhotoUrl: {type: String},
   uCurrency: String,
